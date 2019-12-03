203bbfde8c619c1c939fabc8ed5698c632d9191e
pepesuadealba <joseluissuarezdealba@gmail.com>
2019-12-02 22:21:37 -0600

    Proyecto FINAL

--- activities/project/scala-challenge/src/main/scala/com/iteso/maf/challenge/exercises/problems/Example.scala
+++ activities/project/scala-challenge/src/main/scala/com/iteso/maf/challenge/exercises/problems/Example.scala
@@ -6,21 +6,21 @@ import scala.annotation.tailrec
 case object Example extends Problem {
 
   /**
-    * Fibonacci
-    * Description:
-    * This is an example problem. In a real problem the description should provide a detailed explanation of
-    * the activity. In this case, the problem would consist in providing a recursive fibonacci implementation for the endpoint.
-    * As it can be seen, the functions, classes and endpoint code was provided. You can expect some of these elements
-    * to be provided in future exercises as well.
-    *
-    * Key points:
-    * > Follow the comments in order to understand the relevant parts of these code.
-    * > Running "sbt test" in the console should pass the tests for this example.
-    * > Running "sbt run" should initialize the app. You can make your own get requests at: http://127.0.0.1:8080/problems/example?size=15
-    *
-    * Examples:
-    * This section will provide you with at least one get request example and the expected response.
-    */
+   * Fibonacci
+   * Description:
+   * This is an example problem. In a real problem the description should provide a detailed explanation of
+   * the activity. In this case, the problem would consist in providing a recursive fibonacci implementation for the endpoint.
+   * As it can be seen, the functions, classes and endpoint code was provided. You can expect some of these elements
+   * to be provided in future exercises as well.
+   *
+   * Key points:
+   * > Follow the comments in order to understand the relevant parts of these code.
+   * > Running "sbt test" in the console should pass the tests for this example.
+   * > Running "sbt run" should initialize the app. You can make your own get requests at: http://127.0.0.1:8080/problems/example?size=15
+   *
+   * Examples:
+   * This section will provide you with at least one get request example and the expected response.
+   */
 
   // This class is used as a wrapper to serialize the result.
   // See the JsonSupport trait for the json implementation.
--- activities/project/scala-challenge/src/main/scala/com/iteso/maf/challenge/exercises/problems/Problem4.scala
+++ activities/project/scala-challenge/src/main/scala/com/iteso/maf/challenge/exercises/problems/Problem4.scala
@@ -1,49 +1,50 @@
 package com.iteso.maf.challenge.exercises.problems
 
 import akka.http.scaladsl.server.Route
+import com.iteso.maf.challenge.exercises.problems.Problem5.{Final, IntList, Item}
 
 case object Problem4 extends Problem {
 
   /**
-    * Introduction to options
-    * Description:
-    * This endpoint introduces the concept of scala Options, a commonly known data structure in functional programming.
-    * The main goal is to provide a type safe endpoint that can perform an operation between two integers
-    * (i.e. a, b) defined by the parameter "operation".
-    * The variable "params" contains a scala immutable Map with the parameters of the get request. Use the typesafe
-    * method get (e.g. params.get("key) : Option[String]) to extract the option value of the parameter. In order to
-    * get familiar with the terms, follow these instructions:
-    *
-    * A) Create an implicit class named "StringOps" with a method: def asInt: Option[Int]. This method should
-    * implement a typesafe conversion between a string to an Int (see Problem1 for an example).
-    * - For further information on implicits see: <https://youtu.be/Oij5V7LQJsA>
-    * - For further information on type/implicits classes see: <https://docs.scala-lang.org/overviews/core/implicit-classes.html>
-    *
-    * B) Create a method with the following signature: def calculate(ops: String, a: Int, b: Int): Option[Int].
-    * This method should implement the operations {sum, subtraction, multiplication, division} with pattern matching
-    * and return None for any other operation (string pattern).
-    * - For further information on pattern matching see: <https://youtu.be/1vxIRkYZfmc>
-    *
-    * C) Use for comprehension and map/flatMap as needed in order to extract the values from the parameter map (params)
-    * and perform the operation with the "calculate" method. Create the variable "challengeResponse"
-    * as an Option[Calculation].
-    * - For further information on for-comprehension/map/flatMap with Options see: <https://youtu.be/Mw_Jnn_Y5iA>
-    * - For further information on for-comprehension see: <https://docs.scala-lang.org/tutorials/FAQ/yield.html>
-    * - For further information on map and flatMap see: <book Essential Scala (5.5 Sequencing Computation)>
-    *
-    * Key Points:
-    * > Use pattern matching for the "calculate" method.
-    * > Use for comprehension and/or map and flatMap.
-    * > Note the definition of map and flatMap for an object of type F[A]:
-    * - map(f: A => B) => F[B]
-    * - flatMap(f: A => F[B]) => F[B]
-    *
-    * Examples:
-    * Get Request: /problems/4?a=1&b=2&operation=sum
-    * Response: {"operation":"sum","a":1,"b":2,"result":3}
-    * Get Request: /problems/4?a=1&b=2&operation=summ
-    * Response (bad request): The request contains bad syntax or cannot be fulfilled.
-    */
+   * Introduction to options
+   * Description:
+   * This endpoint introduces the concept of scala Options, a commonly known data structure in functional programming.
+   * The main goal is to provide a type safe endpoint that can perform an operation between two integers
+   * (i.e. a, b) defined by the parameter "operation".
+   * The variable "params" contains a scala immutable Map with the parameters of the get request. Use the typesafe
+   * method get (e.g. params.get("key) : Option[String]) to extract the option value of the parameter. In order to
+   * get familiar with the terms, follow these instructions:
+   *
+   * A) Create an implicit class named "StringOps" with a method: def asInt: Option[Int]. This method should
+   * implement a typesafe conversion between a string to an Int (see Problem1 for an example).
+   * - For further information on implicits see: <https://youtu.be/Oij5V7LQJsA>
+   * - For further information on type/implicits classes see: <https://docs.scala-lang.org/overviews/core/implicit-classes.html>
+   *
+   * B) Create a method with the following signature: def calculate(ops: String, a: Int, b: Int): Option[Int].
+   * This method should implement the operations {sum, subtraction, multiplication, division} with pattern matching
+   * and return None for any other operation (string pattern).
+   * - For further information on pattern matching see: <https://youtu.be/1vxIRkYZfmc>
+   *
+   * C) Use for comprehension and map/flatMap as needed in order to extract the values from the parameter map (params)
+   * and perform the operation with the "calculate" method. Create the variable "challengeResponse"
+   * as an Option[Calculation].
+   * - For further information on for-comprehension/map/flatMap with Options see: <https://youtu.be/Mw_Jnn_Y5iA>
+   * - For further information on for-comprehension see: <https://docs.scala-lang.org/tutorials/FAQ/yield.html>
+   * - For further information on map and flatMap see: <book Essential Scala (5.5 Sequencing Computation)>
+   *
+   * Key Points:
+   * > Use pattern matching for the "calculate" method.
+   * > Use for comprehension and/or map and flatMap.
+   * > Note the definition of map and flatMap for an object of type F[A]:
+   * - map(f: A => B) => F[B]
+   * - flatMap(f: A => F[B]) => F[B]
+   *
+   * Examples:
+   * Get Request: /problems/4?a=1&b=2&operation=sum
+   * Response: {"operation":"sum","a":1,"b":2,"result":3}
+   * Get Request: /problems/4?a=1&b=2&operation=summ
+   * Response (bad request): The request contains bad syntax or cannot be fulfilled.
+   */
 
   case class Calculation(operation: String, a: Int, b: Int, result: Int)
 
@@ -52,19 +53,44 @@ case object Problem4 extends Problem {
       parameterMap {
         params => {
           // <---- Your code starts here. --->
-
           // A) Implicit class here with:
           // def asInt: Option[Int] = ???
-          ???
-
+          implicit class StringOps(x: String){
+            def asInt: Option[Int] = {
+              try {Some(x.toInt)}
+              catch { case _: Throwable => None}
+            }
+          }
           // B) Implement the calculate method.
           // def calculate(ops: String, a: Int, b: Int): Option[Int] = ???
-          def calculate(ops: String, a: Int, b: Int): Option[Int] = ???
-
+          def calculate(ops: String, a: Int, b: Int): Option[Int] =
+            ops match {
+              case "sum" => Some(a+b)
+              case "subtraction" => Some(a-b)
+              case "multiplication" => Some(a*b)
+              case "division" => try {Some(a/b)}
+              catch { case _: Throwable => None}
+              case _ => None
+            }
           // C) Complete the challenge response variable.
           // val challengeResponse: Option[Calculation] = ???
-          val challengeResponse: Option[Calculation] = ???
 
+          val optA: Option[Int] = params.get("a").flatMap(a => a.asInt)
+
+          val optB: Option[Int] = params.get("b").flatMap(b => b.asInt)
+
+          val optOp: Option[String] = params.get("operation")
+
+          //val res: Option[Calculation] = optOp.flatMap(op => optA.flatMap(a => optB.flatMap(b => calculate(op, a, b).map(c => Calculation(op, a, b, c)))))
+
+          val res = for {
+            a <- optA
+            b <- optB
+            op <- optOp
+            c <- calculate(op, a, b)
+          } yield Calculation(op, a, b, c)
+
+          val challengeResponse: Option[Calculation] = res
           // <---- Your code ends  here. ---->
           challengeResponse match {
             case None => badRequestResponse
@@ -74,5 +100,4 @@ case object Problem4 extends Problem {
       }
     }
   }
-
 }
